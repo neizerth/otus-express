888eaddd589fc151fcd3879a18e4937bfd2a03b0
Vladimir Yazykov <neizerth@gmail.com>
2026-03-16 22:07:46 +0300

    feat: file upload

--- 09-file-upload.ts
+++ 09-file-upload.ts
@@ -2,20 +2,32 @@
 import express from "express";
 import multer from "multer";
 import cors from "cors";
+import * as path from "node:path";
 
-const upload = multer({ dest: "uploads/" });
+const storage = multer.diskStorage({
+	destination(_req, _file, cb) {
+		cb(null, "./uploads/");
+	},
+	filename(_req, file, cb) {
+		cb(
+			null,
+			`${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`,
+		);
+	},
+});
+
+const upload = multer({ storage });
 
 const PORT = 3000;
 const app = express();
 
 app.use(cors());
-
-// http://localhost:3000/
+app.use(express.json());
 app.post("/upload", upload.single("avatar"), (req, res) => {
-  res.send({
-    time: new Date(),
-    file: req.file,
-  });
+	res.send({
+		time: new Date(),
+		file: req.file,
+	});
 });
 
 app.listen(PORT, () => console.log(`Hello from ${PORT}`));
